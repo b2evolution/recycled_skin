78f2faa6edcdcd28b2d5f8f2e26c6f098cf75ffa
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-10-18 23:13:57 +0200

    skin version fix + new skinshot

--- _skin.class.php
+++ _skin.class.php
@@ -13,7 +13,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class recycled_Skin extends Skin
 {
-	var $version = '1.0';
+	var $version = '1.1.1';
 
     /**
 	 * Get default name for the skin.
@@ -31,6 +31,33 @@ class recycled_Skin extends Skin
 	function get_default_type()
 	{
 		return 'normal';
+	}
+	
+
+	/**
+	* Get supported collection kinds.
+	*
+	* This should be overloaded in skins.
+	*
+	* For each kind the answer could be:
+	* - 'yes' : this skin does support that collection kind (the result will be was is expected)
+	* - 'partial' : this skin is not a primary choice for this collection kind (but still produces an output that makes sense)
+	* - 'maybe' : this skin has not been tested with this collection kind
+	* - 'no' : this skin does not support that collection kind (the result would not be what is expected)
+	* There may be more possible answers in the future...
+	*/
+	public function get_supported_coll_kinds()
+	{
+		$supported_kinds = array(
+			'main'   => 'no',
+			'std'    => 'yes',		// Blog
+			'photo'  => 'no',
+			'forum'  => 'no',
+			'manual' => 'maybe',
+			'group'  => 'no',  // Tracker
+			// Any kind that is not listed should be considered as "maybe" supported
+		);
+		return $supported_kinds;
 	}
 
 
